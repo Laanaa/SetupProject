2bf5a15f5f753c5fa8d1104c0e09d1224a588d57
laanaa <maulanamalik156457n@gmail.com>
2021-10-14 17:58:40 -0400

    Add activiy menu, order, transaction and config button

--- app/src/main/java/com/example/inder/MainActivity.java
+++ app/src/main/java/com/example/inder/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.inder;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onStartMenuActivity(View view) {
+        Intent myIntent = new Intent(this, Menu.class);
+        startActivity(myIntent);
+    }
+
+    public void onStartTransactionActivity(View view) {
+        Intent myIntent = new Intent(this, Transaction.class);
+        startActivity(myIntent);
+    }
+
+    public void onStartOrderActivity(View view) {
+        Intent myIntent = new Intent(this, Order.class);
+        startActivity(myIntent);
     }
 }
