f25753d9ddb37df0e11d255c373c4350486dff02
Laanaa <maulanamalik156357n@gmail.com>
2021-11-21 10:18:40 -0500

    Refactor main menu intent

--- app/src/main/java/com/example/inder/MainActivity.java
+++ app/src/main/java/com/example/inder/MainActivity.java
@@ -5,15 +5,39 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
+    Button btnMenu, btnTransaction, btnOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMenu = findViewById(R.id.btnMenu);
+        btnTransaction = findViewById(R.id.btnTransaction);
+        btnOrder = findViewById(R.id.btnOrder);
+        btnMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStartMenuActivity(v);
+            }
+        });
+        btnTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStartTransactionActivity(v);
+            }
+        });
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onStartOrderActivity(v);
+            }
+        });
     }
 
     public void onStartMenuActivity(View view) {
